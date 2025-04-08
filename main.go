c86b96c3db3a362ac9d499c9c8ead351bd50d7e7
GuillemotClement <guillemot.clement@protonmail.com>
2025-04-08 13:01:41 +0200

    Feat: add router

--- main.go
+++ main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
+
+	port := ":8086"
+
 	services.InitDB()
+
+	// initialisation du router
+	r := services.SetupRouter()
+
+	// autorisation du proxy
+	r.SetTrustedProxies(nil) // retire l'erreur
+
+	// lancement du serveur
+	r.Run(port)
 }
