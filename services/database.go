cacd283054ce9ab1a27d4fa08870d1faa3e49f18
GuillemotClement <guillemot.clement@protonmail.com>
2025-04-08 12:31:31 +0200

    fix connect db

--- services/database.go
+++ services/database.go
@@ -4,17 +4,21 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+
+	_ "github.com/lib/pq"
 )
 
 // database handler
 var db *sql.DB
 
 func InitDB() {
+	// creation d'une variable eventuelle
+	var err error
 	// capture connection properties
 	dsn := "postgres://postgres:postgres@localhost/hubcook?sslmode=disable"
 
 	// connection a la database
-	db, err := sql.Open("postgres", dsn)
+	db, err = sql.Open("postgres", dsn)
 
 	// tentative de connection a la db
 	if err != nil {
